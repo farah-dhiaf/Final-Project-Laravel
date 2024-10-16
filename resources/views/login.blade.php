f3ff3652d6599c8c9af69f03243fce3b858291df
wamalias <wardamalias@gmail.com>
2024-10-16 12:25:01 +0700

    fix bug

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,7 +14,7 @@
       <div id="alert-failed" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
           <span class="block sm:inline">{{ session('loginError') }}</span>
           <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-              <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" title="Close" onclick="document.getElementById('alert-success').style.display='none';">
+              <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" title="Close" onclick="document.getElementById('alert-failed').style.display='none';">
                   <path d="M14.348 14.849a1 1 0 01-1.414 0L10 11.414l-2.934 2.935a1 1 0 01-1.414-1.415l2.935-2.934-2.935-2.934a1 1 0 011.414-1.414L10 8.586l2.934-2.935a1 1 0 011.414 1.415L11.414 10l2.935 2.934a1 1 0 010 1.415z"/>
               </svg>
           </span>
